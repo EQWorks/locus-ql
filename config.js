2793302ef0dd53f8f6713daf3d50b3ee70f147d5
Jason Feng <IYCI@users.noreply.github.com>
2018-07-24 11:56:43 -0400

    [G2M] stage deployment (#50)
    * stage deployment
    
    * simplify code
    
    * use right keywarden stage
    
    * revert to include slash in path

--- config.js
+++ config.js
@@ -27,6 +27,10 @@ const keyWardenConfig = {
   stage: process.env.KEY_WARDEN_STAGE,
 }
 
+const basePath = process.env.API_GATEWAY_BASE_PATH || ''
+
+const commitHash = process.env.COMMIT_SHORT_HASH || 'unknown'
+
 const googleMapConfig = {
   apiKey: process.env.GOOGLE_MAP_API_KEY,
 }
@@ -37,5 +41,7 @@ module.exports = {
   mapbox: mapboxConfig,
   graphhopper: graphhopperConfig,
   keyWarden: keyWardenConfig,
+  basePath,
+  commitHash,
   googleMap: googleMapConfig,
 }
