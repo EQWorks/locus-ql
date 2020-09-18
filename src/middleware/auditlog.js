e24b374c59734fe6b60b93c7d3b71f0ae1760399
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-09-18 17:42:42 -0400

    audit log - insertion retry logic

--- src/middleware/auditlog.js
+++ src/middleware/auditlog.js
@@ -6,27 +6,33 @@ const { pool } = require('../util/db')
 const insertLog = (...values) => (_, res) => {
   // local "STAGE"-less
   if (!process.env.API_GATEWAY_BASE_PATH) {
-    return console.log('Local Audit Log:', ...values)
+    return console.log('Local Audit Log:', ...values, res.statusCode, res.return_meta)
   }
   // remote deployment
-  return pool.connect()
-    .then(client => client.query({
-      text: `
-        INSERT INTO locus_log(
-          email,
-          time_st,
-          action,
-          payload,
-          http_method,
-          api_path,
-          return_code,
-          return_meta
-        )
-        VALUES ($1, now(), $2, $3, $4, $5, $6, $7);
-      `,
-      values: [...values, res.statusCode, res.return_meta],
-    }).finally(() => client.release())) // the finally is intentionally in client scope
-    .catch(console.error)
+  function insert() {
+    return pool.connect()
+      .then(client => client.query({
+        text: `
+          INSERT INTO locus_log(
+            email,
+            time_st,
+            action,
+            payload,
+            http_method,
+            api_path,
+            return_code,
+            return_meta
+          )
+          VALUES ($1, now(), $2, $3, $4, $5, $6, $7);
+        `,
+        values: [...values, res.statusCode, res.return_meta],
+      }).then(() => client.release())) // intentionally in client scope
+  }
+  let p = Promise.reject() // promise retry hack https://stackoverflow.com/a/38225011/158111
+  for (let i = 0; i < 3; i++) { // max 3 retries
+    p = p.catch(insert).then(() => null)
+  }
+  return p.catch(console.error)
 }
 
 module.exports.auditlog = (action = 'others') => (req, res, next) => {
