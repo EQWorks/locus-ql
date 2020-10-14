ec3551cdb1d70a665e7c2107d4d930c350fad947
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-10-14 16:33:01 -0400

    activity/popular-times - smaug rate limit 750/min

---
+++ src/util/billing.js
@@ -0,0 +1,19 @@
+const { lambda } = require('./aws')
+
+
+// reference from smaug service
+// CONFIG_TYPES = {
+//   'minute': (int, -1),
+//   'hour': (int, -1),
+//   'day': (int, -1),
+//   'month': (int, -1),
+//   'white-label': (str, None),
+//   'customer': (str, None),
+//   'user': (str, None),
+//   'prefix': (str, None),
+// }
+module.exports.increment = ({ config, n = 1 }) => lambda.invoke({
+  FunctionName: 'smaug-dev-increment', // TODO: wire-in different stages
+  InvocationType: 'RequestResponse',
+  Payload: JSON.stringify({ config, n }),
+}).promise()
