e00074b467356e9cc8a62891f4e1702f5f3332ed
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-07-07 14:25:02 -0400

    auth - include original token in req.access for relay cases

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,10 +12,10 @@ const KEY_WARDEN_BASE = `${KEY_WARDEN_HOST}/${KEY_WARDEN_STAGE}`
 
 function jwtMiddleware(req, _, next) {
   // DEPRECATED `x-firstorder-token`
-  const uJWT = req.get('eq-api-jwt') || req.get('x-firstorder-token')
+  const token = req.get('eq-api-jwt') || req.get('x-firstorder-token')
 
   // quick validation
-  if (!(req.headers && uJWT && uJWT.length > 0)) {
+  if (!(req.headers && token && token.length > 0)) {
     return next(apiError('Invalid JWT', 401))
   }
 
@@ -24,7 +24,7 @@ function jwtMiddleware(req, _, next) {
     api_access = {},
     prefix,
     product: tokenProduct,
-  } = req.authorizerAccess || jwt.decode(uJWT)
+  } = req.authorizerAccess || jwt.decode(token)
 
   const { write = 0, read = 0 } = api_access
   let { wl: whitelabel = [], customers = [] } = api_access
@@ -48,7 +48,7 @@ function jwtMiddleware(req, _, next) {
     }
   }
 
-  req.access = { whitelabel, customers, write, read, email, prefix }
+  req.access = { whitelabel, customers, write, read, email, prefix, token }
 
   const product = _product && ['atom', 'locus'].includes(_product) ? _product : 'locus'
 
@@ -63,7 +63,7 @@ function jwtMiddleware(req, _, next) {
   axios({
     url: `${KEY_WARDEN_BASE}/confirm`,
     method: 'get',
-    headers: { 'eq-api-jwt': uJWT },
+    headers: { 'eq-api-jwt': token },
     params: { product },
   }).then(() => next()).catch(next)
 }
