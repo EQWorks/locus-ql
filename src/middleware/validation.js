1e408cc4c540134b4a01f8be8bb41e8db7cb5b00
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-07-20 17:52:57 -0400

    middleware/validation - revise OneOf.prototype.toString() to replace .show()

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -11,11 +11,11 @@ class OneOf {
   validate(obj) {
     return this.params.some(p => Object.prototype.hasOwnProperty.call(obj, p))
   }
-  show() {
+  toString() {
     if (this.params.length > 1) {
-      return `one of ${this.params.join(', ')}`
+      return `one of ${this.params.toString()}`
     }
-    return this.params[0]
+    return this.params.toString() // ['one'].toString() => 'one'
   }
 }
 module.exports.oneOf = (...params) => new OneOf(...params)
@@ -23,7 +23,7 @@ module.exports.oneOf = (...params) => new OneOf(...params)
 const _hasParams = target => (...params) => (req, _, next) => {
   for (const param of params) {
     if (!new OneOf(param).validate(req[target])) {
-      return next(apiError(`Missing ${param.show()} in ${target}`, 400))
+      return next(apiError(`Missing ${param.toString()} in ${target}`, 400))
     }
   }
   return next()
