0439e1de9f82aa22dfb899d5030cd38798a0162e
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-07-20 13:03:16 -0400

    middleware/validation - OneOf and its validation() for all single params

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -3,24 +3,27 @@ const { apiError } = require('../util/api-error')
 
 class OneOf {
   constructor(...params) {
+    if (params[0] instanceof OneOf) { // ignore others if first is a OneOf instance
+      return params[0] // short-circuit no-op return
+    }
     this.params = params
   }
   validate(obj) {
     return this.params.some(p => Object.prototype.hasOwnProperty.call(obj, p))
   }
   show() {
-    return `one of ${this.params.join(', ')}`
+    if (this.params.length > 1) {
+      return `one of ${this.params.join(', ')}`
+    }
+    return this.params[0]
   }
 }
 module.exports.oneOf = (...params) => new OneOf(...params)
 
 const _hasParams = target => (...params) => (req, _, next) => {
   for (const param of params) {
-    if (param instanceof OneOf && !param.validate(req[target])) {
+    if (!new OneOf(param).validate(req[target])) {
       return next(apiError(`Missing ${param.show()} in ${target}`, 400))
-    }
-    if (!req[target][param]) {
-      return next(apiError(`Missing '${param}' in ${target}`, 400))
     }
   }
   return next()
