f2fee2bd164f620e4aba155bc5d03467b2f56a63
Pei-Yi Lin <amy83762100@gmail.com>
2023-11-08 14:51:11 -0500

    trino - update Trino client configuration to include port from environment variable

--- src/util/trino.js
+++ src/util/trino.js
@@ -4,6 +4,7 @@ const { Client } = require('@eqworks/trino-client-node')
 // instantiate client
 module.exports = new Client({
   host: process.env.TRINO_HOST,
+  port: process.env.TRINO_PORT,
   username: process.env.TRINO_USERNAME,
   password: process.env.TRINO_PASSWORD,
 })
